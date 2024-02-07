a57ca342c524dc704b187e0c624f406902917d41
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-02-07 20:16:09 +0600

    problem_1.js done

--- problem_1.js
+++ problem_1.js
@@ -1,1 +1,15 @@
-console.log("hello");
+function calculateMoney(ticketSale) {
+    
+    // if the ticket number is not valid
+    if (ticketSale <= 0) { 
+        return "Please provide a valid number"
+    }
+
+    // if the ticket number is valid
+    var profit = (ticketSale * 120) - (500 + (8 * 50));
+    return profit;
+}
+
+var profit = calculateMoney(-130);
+
+console.log(profit);
